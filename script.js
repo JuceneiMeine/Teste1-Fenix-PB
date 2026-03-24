c8b34635d5b75573e76df0130e051540e1ecee3c
JuceneiMeine <jucenei.meine@gmail.com>
2026-03-23 23:18:21 -0300

    Add files via upload
    ATT

--- script.js
+++ script.js
@@ -17,39 +17,14 @@ document.querySelectorAll('.nav-menu a').forEach(link => {
 });
 
 
-
-// 2. Sistema de Modal para Atividades
-const modal = document.getElementById('infoModal');
-const modalTitle = document.getElementById('modalTitle');
-const modalBody = document.getElementById('modalBody');
-const closeModal = document.querySelector('.close-modal');
-const activityLinks = document.querySelectorAll('.activity-link');
-
-activityLinks.forEach(link => {
-    link.addEventListener('click', (e) => {
-        e.preventDefault();
-        const title = link.getAttribute('data-title');
-        const info = link.getAttribute('data-info');
-        
-        modalTitle.innerText = title;
-        modalBody.innerText = info;
-        modal.style.display = 'flex';
-    });
-});
-
-closeModal.addEventListener('click', () => {
-    modal.style.display = 'none';
 });
 
 
 
-// Fechar modal ao clicar fora do conteúdo
-window.addEventListener('click', (e) => {
-    if (e.target === modal) {
-        modal.style.display = 'none';
+let index = 0;
+    function moveSlide(step) {
+        const slides = document.querySelectorAll('.slide');
+        index = (index + step + slides.length) % slides.length;
+        document.getElementById('slideTrack').style.transform = `translateX(${-index * 100}%)`;
     }
-});
-});
-
-
 
